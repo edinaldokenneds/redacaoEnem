02c4c3e4ddd1e074dfabaaf2130d8609dcfd8fba
Renan Ferreira <79757077+meiotera@users.noreply.github.com>
2022-07-13 08:11:27 -0300

    Update script.js
    Alteração de funcionalidades

--- js/script.js
+++ js/script.js
@@ -1,34 +1,39 @@
 let perguntaAtual = 0;
 let opt = [];
-
+let btInicar = document.getElementById('btInicar');
 mostrarPergunta(); 
 
+btInicar.addEventListener('click', ()=>{
+    let paginaTemaRedacao = document.querySelector('.temaRedacao');
+    paginaTemaRedacao.style.display = 'none'
+})
 function mostrarPergunta() {
     if(perguntas[perguntaAtual]) {
         let perg = perguntas[perguntaAtual];
-
         let pct = Math.floor((perguntaAtual / perguntas.length) * 100);
 
-        document.querySelector('.barra').style.width = `${pct}%`;
-        
+        document.querySelector('.barra').style.width = `${pct}%`;        
         document.querySelector('.success_info').style.display = 'none';
         document.querySelector('.question_area').style.display = 'block';
-
         document.querySelector('.question').innerHTML = perg.pergunta;
    
         let op = '';
 
+        if(perguntaAtual > 0){
+            document.getElementById('btVoltar').style.display = 'block';
+        } else {
+            document.getElementById('btVoltar').style.display = 'none';
+        }
         
         for(let i in perg.options) {
             if(perguntaAtual === 5) {
-                op += `<label><input value="${perg.options[i]}" type="checkbox" class="checkbox" name="options" id="options"><span>${perg.options[i]}</span></input></label><br>`;               
-                                           
+                op += `<label><input value="${perg.options[i]}" type="checkbox" name="options"><span>${perg.options[i]}</span></input></label><br>`;    
+
             }else{
                 op += `<label><input value="${perg.options[i]}" type="radio" name="options" id="options"><span>${perg.options[i]}</span></input></label><br>`;
             }
         }
-        document.querySelector('.options').innerHTML = op;  
-        
+        document.querySelector('.options').innerHTML = op;       
         
     } else {
         fim(opt);
@@ -38,34 +43,33 @@ function mostrarPergunta() {
 let btAvancar = document.getElementById('btAvancar');
 btAvancar.addEventListener('click', ()=>{
     let checked = document.querySelectorAll('.options input');    
-    let tituloQuestao = document.querySelector('.question span');
-    
+    let tituloQuestao = document.querySelector('.question span');  
+
     let op = null;
     
     for(check of checked){        
         if(check.checked){
-            op = check.value
+            op = check.value;
             opt.push(`${tituloQuestao.innerText.bold()} = ${check.value}`);
         }
     }
 
     if(op === null){
-        modal() 
-        return
-    }
-    console.log(opt)
-    
-    perguntaAtual++
+        modal();
+        return;
+    }   
+
+    console.log(opt);    
+    perguntaAtual++;
     mostrarPergunta();
 });
-
 
 function fim(opt){
     document.querySelector('.success_info').style.display = 'block';
     document.querySelector('.question_area').style.display = 'none';
     document.querySelector('.barra').style.width = `100%`;
-    //opt.replace('::', ':')
-    let vescolhas =  ''
+
+    let vescolhas =  '';
     
     for(op of opt){
         vescolhas += `<p>${op}</p>`;
@@ -76,13 +80,20 @@ function fim(opt){
 let btReiniciar = document.getElementById('btReiniciar');
 btReiniciar.addEventListener('click', () => {    
     perguntaAtual = 0;
-    opt = []
+    opt = [];
     mostrarPergunta();
-})
+});
 
 function modal(){
     let modal = document.getElementById('staticBackdrop');
     let minhaModal = new bootstrap.Modal(modal)
     minhaModal.show();
-    return
+    return;
 }
+
+let btVoltar = document.getElementById('btVoltar');
+btVoltar.addEventListener('click', () => {
+    perguntaAtual--;
+    mostrarPergunta();
+    opt.splice(-1, 1);    
+});
