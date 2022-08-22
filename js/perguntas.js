73ecebe95ef1586286f1eb81943ad447f181c314
Renan Ferreira <renanfc.re@gmail.com>
2022-08-22 09:28:44 -0300

    Alteração de tema
    Nova logo inserida, novo tema inserido

--- js/perguntas.js
+++ js/perguntas.js
@@ -370,5 +370,182 @@ let perguntas = [
             ]
         },
 
+    ],
+
+
+    [
+        {
+            pergunta: 'Introdução <br> <span>1.1 CONTEXTUALIZAÇÃO </span>',
+            options: [
+                'FILME “WALL-E”',
+                'CONSTITUIÇÃO FEDERAL DE 1988',
+                'DOCUMENTÁRIO MINIMALISTA',                
+            ]    
+        },
+        {        
+            pergunta: 'Introdução <br> <span> 1.2 APRESENTAÇÃO DO TEMA: ARTICULADORES</span>',        
+            options: [
+                'INFELIZMENTE', 
+                'ENTRETANTO', 
+                'PORÉM', 
+                'ASSIM', 
+                'DESSE MODO'
+            ]
+        },
+        {
+            pergunta: 'Introdução <br> <span> 1.2 APRESENTAÇÃO DO TEMA: LOCAL</span>',
+            options: [
+                'BRASIL', 
+                'PAÍS',
+                'NAÇÃO BRASILEIRA'
+            ]
+        },
+        {
+            pergunta: 'Introdução <br> <span> 1.2 APRESENTAÇÃO DO TEMA: TEMPO</span>',
+            options: [
+                'HODIERNAMENTE',
+                'DIAS ATUAIS', 
+                'ATUALMENTE'
+            ]
+        },
+        {
+            pergunta: 'Introdução <br> <span> 1.2 APRESENTAÇÃO DO TEMA: VERBO</span>',
+            options: [
+                'REPRESENTA', 
+                'É', 
+                'CONFIGURA'
+            ]
+        },
+        {
+            pergunta: 'Introdução <br> <span> 1.3 TESE </span>',
+            options: [
+                'NEGLIGÊNCIA GOVERNAMENTAL',
+                'CULTURA DO CONSUMISMO',
+                'SOCIEDADE CONSUMISTA',
+                'OBSOLESCÊNCIA PROGRAMADA'
+            ]       
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 1 <br> <span> 2.1 CONECTIVOS </span>',
+            options: [
+                'DIANTE DESSE CENÁRIO',
+                'DIANTE DESSA REALIDADE',
+                'SOB ESSE VIÉS',
+                'NESSA PERSPECTIVA'
+            ]        
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 1 <br> <span> 2.2 RETOMADA DA TESE </span>',
+            options: [
+                'FALTA DE INVESTIMENTO ESTATAL',
+                'CONSUMISMO EXAGERADO',
+                'CIDADÃOS NÃO POLITIZADOS',
+                'CONSUMO DE MASSA'
+            ]        
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 2 <br> <span> 2.3 EXEMPLIFICAÇÃO </span>',
+            options: [
+                'A RESPONSABILIDADE SOCIAL E A PRESERVAÇÃO AMBIENTAL SIGNIFICA UM COMPROMISSO COM A VIDA. “O HOMEM É O LOBO DO HOMEM”. —  THOMAS HOBBES',
+                '"O HOMEM NÃO É NADA ALÉM DAQUILO QUE A EDUCAÇÃO FAZ DELE." (KANT)                NA NATUREZA NADA SE CRIA, NADA SE PERDE, TUDO SE TRANFORMA  (ANTOINE LAVORSIER)',               
+            ]
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 1 <br> <span> 2.4 COMENTÁRIO - CONECTIVO </span>',
+            options: [
+                'DESSE MODO',
+                'ASSIM',
+                'DESSA FORMA',
+                'DIANTE DISSO',
+                'LOGO',
+                'NESSE SENTIDO'
+            ]
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 2 <BR> <span> 3.1 CONECTIVOS </span>',
+            options: [
+                'ALÉM DISSO',
+                'ADEMAIS',
+                'SOMA-SE A ISSO',
+            
+            ]
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 2 <br> <span> 3.2 EXEMPLIFICAÇÃO </span>',
+            options: [
+                'A RESPONSABILIDADE SOCIAL E A PRESERVAÇÃO AMBIENTAL SIGNIFICA UM COMPROMISSO COM A VIDA. (JOÃO BOSCO SILVA)',
+                'O HOMEM É O LOBO DO HOMEM.“ —  THOMAS HOBBES',
+                '"O HOMEM NÃO É NADA ALÉM DAQUILO QUE A EDUCAÇÃO FAZ DELE." (KANT)',
+                'NA NATUREZA NADA SE CRIA, NADA SE PERDE, TUDO SE TRANFORMA  (ANTOINE LAVORSIER)'            
+            ]
+        },   
+        {
+            pergunta: 'DESENVOLVIMENTO 2 <br> <span> 3.4 COMENTÁRIO CONECTIVO </span>',
+            options: [
+                'DESSE MODO',
+                'ASSIM',
+                'DESSA FORMA',
+                'DIANTE DISSO',
+                'LOGO',
+                'NESSE SENTIDO'    
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.1 CONECTIVOS </span>',
+            options: [
+                'PORTANTO',
+                'DESTARTE',
+                'EM SUMA'             
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.2  AGENTE </span>',
+            options: [
+                'MINISTÉRIO DO MEIO AMBIENTE',
+                'MINISTÉRIO DA EDUCAÇÃO',
+                'ESCOLA',
+                'MÍDIA',
+                'SOCIEDADE'          
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.3 DETALHAMENTO DO AGENTE </span>',
+            options: [
+                'ÓRGÃO RESPONSÁVEL PELAS POLÍTICAS PÚBLICAS NA ÁREA DO MEIO AMBIENTE',
+                'ÓRGÃO RESPONSÁVEL PELAS POLÍTICAS PÚBLICAS NA ÁREA DA EDUCACIONAL',
+                'GRANDE DIFUSORA DE INFORMAÇÃO E VEÍCULO FORMADOR DE OPINIÃO',
+                'ENTIDADE RESPONSÁVEL PELA EDUCAÇÃO FORMAL',
+                       
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.4 AÇÃO </span>',
+            options: [
+                'DEVE CRIAR',
+                'DEVE AMPLIAR',
+                'DEVE INFORMAR',
+                'DEVE INVESTIR'                      
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.5 MEIO MODO </span>',
+            options: [
+                'POR MEIO DE PALESTRAS',
+                'POR MEIO DE RECURSOS',
+                'POR MEIO DE CAMPANHAS PUBLICITÁRIAS',
+                'POR MEIO DE DEBATES',
+                                  
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.6 FINALIDADE </span>',
+            options: [
+                'A FIM DE AMENIZAR',
+                'A FIM DE DIMINUIR',
+                'A FIM DE ATENUAR',
+                'A FIM DE MITIGAR'                      
+            ]
+        },
+
     ]
 ]
