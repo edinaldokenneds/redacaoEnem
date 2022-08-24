4508d47b9bc5b22fa7cd471226b90a2b0edb677d
edinaldokenneds <edinaldokenneds@gmail.com>
2022-08-24 15:02:23 -0300

    Tema3 teste

--- js/perguntas.js
+++ js/perguntas.js
@@ -547,5 +547,187 @@ let perguntas = [
             ]
         },
 
-    ]
+    ],
+    // tema teste
+    [
+        {
+            pergunta: 'Introdução <br> <span>1.1 CONTEXTUALIZAÇÃO </span>',
+            options: [
+                'teste',
+                'teste 2',
+                'teste 3',
+                'ESTATUTO DA CRIANÇA E DO ADOLESCENTE'
+            ]        
+        },
+        {        
+            pergunta: 'Introdução <br> <span> 1.2 APRESENTAÇÃO DO TEMA: ARTICULADORES</span>',        
+            options: [
+                'INFELIZMENTE', 
+                'ENTRETANTO', 
+                'PORÉM', 
+                'ASSIM', 
+                'DESSE MODO'
+            ]
+        },
+        {
+            pergunta: 'Introdução <br> <span> 1.2 APRESENTAÇÃO DO TEMA: LOCAL</span>',
+            options: [
+                'BRASIL', 
+                'PAÍS',
+                'NAÇÃO BRASILEIRA'
+            ]
+        },
+        {
+            pergunta: 'Introdução <br> <span> 1.2 APRESENTAÇÃO DO TEMA: TEMPO</span>',
+            options: [
+                'HODIERNAMENTE',
+                'DIAS ATUAIS', 
+                'ATUALMENTE'
+            ]
+        },
+        {
+            pergunta: 'Introdução <br> <span> 1.2 APRESENTAÇÃO DO TEMA: VERBO</span>',
+            options: [
+                'REPRESENTA', 
+                'É', 
+                'CONFIGURA'
+            ]
+        },
+        {
+            pergunta: 'Introdução <br> <span> 1.3 TESE </span>',
+            options: [
+                'NEGLIGÊNCIA GOVERNAMENTAL',
+                'FALTA DE INFORMAÇÃO',
+                'AUSÊNCIA DE DIÁLOGO NA FAMÍLIA'
+            ]       
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 1 <br> <span> 2.1 CONECTIVOS </span>',
+            options: [
+                'DIANTE DESSE CENÁRIO',
+                'DIANTE DESSA REALIDADE',
+                'SOB ESSE VIÉS',
+                'NESSA PERSPECTIVA'
+            ]        
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 1 <br> <span> 2.2 RETOMADA DA TESE </span>',
+            options: [
+                'FALTA DE INVESTIMENTO ESTATAL',
+                'DESINFORMAÇÃO',
+                'AUSÊNCIA DE COMUNICAÇÃO',
+            ]        
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 1 <br> <span> 2.3 EXEMPLIFICAÇÃO </span>',
+            options: [
+                '44 NASCIMETO PARA CADA MIL ADOLESCENTE (MINISTÉRIO DA SAÚDE)',
+                'THOMAS HOBBES “O ESTADO É RESPONSÁVEL PELO BEM-ESTAR SOCIAL DE UM POVO',
+                'IMMANUEL KANT “O HOMEM É AQUILO QUE A EDUCAÇÃO FAZ DELE”'
+            ]
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 1 <br> <span> 2.4 COMENTÁRIOCONECTIVO </span>',
+            options: [
+                'DESSE MODO',
+                'ASSIM',
+                'DESSA FORMA',
+                'DIANTE DISSO',
+                'LOGO',
+                'NESSE SENTIDO'
+            ]
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 2 <BR> <span> 3.1 CONECTIVOS </span>',
+            options: [
+                'ALÉM DISSO',
+                'ADEMAIS',
+                'SOMA-SE A ISSO',
+            
+            ]
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 1 <br> <span> 3.2 RETOMADA DA TESE </span>',
+            options: [
+                'FALTA DE INVESTIMENTO ESTATAL',
+                'DESINFORMAÇÃO',
+                'AUSÊNCIA DE COMUNICAÇÃO',
+            
+            ]
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 2 <br> <span> 3.3 EXEMPLIFICAÇÃO </span>',
+            options: [
+                'DIÁRIO DE ANNE FRANK',
+                '70% DAS VÍTIMAS DE ESTUPROS SÃO CRIANÇAS E ADOLESCENTES',
+                'ARISTÓTELES “ TODOS OS HOMENS TÊM, POR NATUREZA, O DESEJO DE CONHECER',
+                'TALCOTT PARSON “A FAMÍLIA É UMA MÁQUINA DE PERSONALIDADE'           
+            ]
+        },
+        {
+            pergunta: 'DESENVOLVIMENTO 2 <br> <span> 3.4 COMENTÁRIO CONECTIVO </span>',
+            options: [
+                'DESSE MODO',
+                'ASSIM',
+                'DESSA FORMA',
+                'DIANTE DISSO',
+                'LOGO',
+                'NESSE SENTIDO'    
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.1 CONECTIVOS </span>',
+            options: [
+                'PORTANTO',
+                'DESTARTE',
+                'EM SUMA'             
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.2  AGENTE </span>',
+            options: [
+                'MINISTÉRIO DA SAÚDE',
+                'MINISTÉRIO DA EDUCAÇÃO',
+                'ESCOLA',
+                'FAMÍLIA'            
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.3 DETALHAMENTO DO AGENTE </span>',
+            options: [
+                'ÓRGÃO RESPONSÁVEL PELAS POLÍTICAS PÚBLICAS NA ÁREA DE SAÚDE',
+                'ÓRGÃO RESPONSÁVEL PELAS POLÍTICAS PÚBLICAS NA ÁREA DA EDUCACIONAL',
+                'GRANDE DIFUSORA DE INFORMAÇÃO E VEÍCULO FORMADOR DE OPINIÃO',
+                'INSTITUIÇÃO RESPONSÁVEL PELA EDUCAÇÃO PRIMÁRIA DOS INDIVÍDUOS',
+                'ENTIDADE RESPONSÁVEL PELA EDUCAÇÃO FORMAL'            
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.4 AÇÃO </span>',
+            options: [
+                'DEVE CRIAR',
+                'DEVE AMPLIAR',
+                'DEVE INFORMAR',
+                'DEVE INVESTIR'                      
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.5 MEIO MODO </span>',
+            options: [
+                'POR MEIO DE PALESTRAS',
+                'POR MEIO DE RECURSOS',
+                'POR MEIO DE CAMPANHAS PUBLICITÁRIAS',
+                'POR MEIO DE DEBATES'                      
+            ]
+        },
+        {
+            pergunta: 'CONCLUSÃO <br> <span> 4.6 FINALIDADE </span>',
+            options: [
+                'A FIM DE AMENIZAR',
+                'A FIM DE DIMINUIR',
+                'A FIM DE ATENUAR',
+                'A FIM DE MITIGAR'                      
+            ]
+        },
+    ],
 ]
